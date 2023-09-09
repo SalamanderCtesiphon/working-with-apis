b45d0f185f0601d35d7cb473b6f1349392792ac7
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-09 13:49:35 -0500

    copied my api key over into my env file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // MEET FETCH FETCH IS YOUR  FRIEND
 
-fetch('https://url.com/some/url')
-  .then(fucntion(response) {
+fetch('https://url.com/some/url').then(fucntion(response) {
     // Successful response 
   })
   .catch(function(err) {
