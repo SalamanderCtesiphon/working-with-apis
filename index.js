07961d21ca31a1d3a818c210164a2afc42555876
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-09 14:47:53 -0500

    implemented a search option although it is a little jank

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // giphy gif api
 // api.giphy.com/v1/gifs/translate
 
+
+
+const selectionBtn = document.getElementById('selection-btn')
+
+selectionBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const selection = document.getElementById('selection').value
+
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=FKHrnl8qaMWyeJf85XwRk8AKF6GmD3Zr&s=${selection}`, {
+  mode: 'cors'
+})
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(response) {
+    img.src = response.data.images.original.url
+  })
+  .catch(function(err) {
+    console.log(err)
+  })
+})
+
 const img = document.querySelector("img")
 
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=FKHrnl8qaMWyeJf85XwRk8AKF6GmD3Zr&s=cats', {
