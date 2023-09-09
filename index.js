979107ecda889044d252f21a89545a6f74551e60
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-09 14:07:57 -0500

    implemented the basic http fetch request. generated the response dot json and the console logged the respone

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // giphy gif api
 // api.giphy.com/v1/gifs/translate
 
+const img = document.querySelector("img")
+
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=FKHrnl8qaMWyeJf85XwRk8AKF6GmD3Zr&s=cats', {
   mode: 'cors'
+})
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(response) {
+  console.log(response)
 })
